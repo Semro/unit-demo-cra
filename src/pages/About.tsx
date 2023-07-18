1cb052a67ee540b8bbe1c15f8f044b2a8f09d9a7
Semyon Rozhkov <semro@outlook.com>
2023-07-18 21:12:41 +0300

    feat: add new paragraphs

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,6 +9,9 @@ export const About: FC = () => {
       </Helmet>
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
+      <p>New paragraph 1<</p>
+      <p>New paragraph 2</p>
+      <p>New paragraph 3</p>
     </>
   );
 };
