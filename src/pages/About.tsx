18b39882b9b1cc1d90746c05bbe4e3669d1eb276
Semyon Rozhkov <semro@outlook.com>
2023-07-18 21:14:27 +0300

    refactor: use loop

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,9 +9,7 @@ export const About: FC = () => {
       </Helmet>
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
-      <p>New paragraph 1</p>
-      <p>New paragraph 2</p>
-      <p>New paragraph 3</p>
+      {Array(3).fill(0).map((_, i) => <p>New paragraph i</p>)}
     </>
   );
 };
